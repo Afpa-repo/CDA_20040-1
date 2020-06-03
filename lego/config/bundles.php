5e9dcc2bd61bf48710e4e3420445e9db9b85b0b8
Paul Dacheux <58972637+Paulo60650@users.noreply.github.com>
2020-06-03 13:59:57 +0200

    test2

--- lego/config/bundles.php
+++ lego/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Flosch\Bundle\StripeBundle\FloschStripeBundle::class => ['dev' => true, 'test' => true],
 ];
